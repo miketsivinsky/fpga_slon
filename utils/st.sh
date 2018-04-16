ea0473423256ac8d55f2cc6b17eb1e03e03a1fe2
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2018-04-16 14:11:55 +0700

    add comment to 'vivado tcl' script

--- utils/st.sh
+++ utils/st.sh
@@ -9,7 +9,7 @@ PRJ_NAME=$1
 PRJ_TCL_FILE=$PRJ_NAME.tcl
 
 if [ -f $PRJ_TCL_FILE ]; then
-	echo "[INFO] $PRJ_NAME started"
+	echo "[INFO] $PRJ_NAME started at tcl mode"
 	$VIVADO/vivado -mode tcl -journal $PRJ_NAME.jou -log $PRJ_NAME.log -source $PRJ_TCL_FILE -notrace
 else
 	echo "[WARN] $PRJ_TCL_FILE not exist"
